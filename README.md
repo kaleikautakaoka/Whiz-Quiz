73d074680d1bc666fbb3ada4b28452238e2142fa
kaleikautakaoka <107520051+kaleikautakaoka@users.noreply.github.com>
2023-04-12 21:17:18 -0600

    q and a object

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Whiz-Quiz
 Whiz Quiz is a resource for anyone who wants to learn Javascript. This site contains a study/resource page and a short quiz on Javascript.
 
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+
 ## Project URL
 
 ## Deployment
@@ -11,7 +23,7 @@ Whiz Quiz is a resource for anyone who wants to learn Javascript. This site cont
 
 <table>
 <tr>
-<td>Ab3 CodeWMe Page Preview</td>
+<td>Whiz Quiz</td>
 </tr>
 <tr>
 <td><img src="./assets/images/AB3.png" alt="screenshot of webpage"></td>
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,45 @@
-//create get element by ID variables to target html elements in javascript
-var startBtn = $('#submit-btn');
-var timerNumber = $('#timer-btn');
+const quizForm = document.querySelector('.quiz-form');
+const a = document.getElementById('checkbox1');
+const b = document.getElementById('checkbox2');
+const c = document.getElementById('checkbox3');
+let questions = document.getElementById('questions');
+// questions.append(qAndAObject.question[0]);
 
-startBtn.on('click', function () {
-    var random = Math.floor(Math.random() * 1000) + 1000;
-    console.log(random);
-    timerNumber.text(random);
+
+const qAndAObject = [
+    { question : "What is a function?",
+     options : [
+    { a: "A statement that performs a task"}, 
+    { b: "A progrmming language"}, 
+    { c: "An array"}, ],
+    answer : a,
+},
+    { question : 'What is an array?',
+     options : [
+    { a: "A statement that performs a task"}, 
+    { b: "A progrmming language"}, 
+    { c: "A list of stored values and elements"}, ], 
+    answer : c,
+},
+    { question : 'What is a class?',
+     options : [
+    { a: "A statement that performs a task"}, 
+    { b: "A template for creating objects"}, 
+    { c: "An array"}, ],
+    answer : b,
+},
+];
+
+
+quizForm.addEventListener('submit', e=> {
+    e.preventDefault();
+    // console.log(firstCheckBox.value)
+//    console.log(questions.append(qAndAObject.question[0]))
+   let score ="0";
+questions.appendChild(qAndAObject.question[0])
+for(i = 0; i < qAndAObject.question.length; i++) {
+    console.log(questions)
+}
+  
+
 });
