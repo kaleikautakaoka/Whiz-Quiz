f6a1f984c6eebb7cf99be859a92b60b10ece48f0
kaleikautakaoka <107520051+kaleikautakaoka@users.noreply.github.com>
2023-04-12 22:21:47 -0600

    functions outlines

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,13 @@
 const quizForm = document.querySelector('.quiz-form');
-const a = document.getElementById('checkbox1');
-const b = document.getElementById('checkbox2');
-const c = document.getElementById('checkbox3');
+const aBox = document.getElementById('checkbox1');
+const bBox = document.getElementById('checkbox2');
+const cBox = document.getElementById('checkbox3');
+const aOption = document.getElementById('form-check-labela');
+const bOption = document.getElementById('form-check-labelb');
+const cOption = document.getElementById('form-check-labelc');
 let questions = document.getElementById('questions');
+const submitButton = document.getElementById('submitbtn');
+const allFormAnswers = document.querySelectorAll('form-check-input');
 // questions.append(qAndAObject.question[0]);
 
 
@@ -30,16 +35,36 @@ const qAndAObject = [
 },
 ];
 
-
+beginQuiz()
 quizForm.addEventListener('submit', e=> {
     e.preventDefault();
     // console.log(firstCheckBox.value)
 //    console.log(questions.append(qAndAObject.question[0]))
-   let score ="0";
-questions.appendChild(qAndAObject.question[0])
-for(i = 0; i < qAndAObject.question.length; i++) {
-    console.log(questions)
-}
-  
+   let qDisplayed = 0; 
+   let score = 0;
 
-});
+   function beginQuiz(){
+    uncheckBox()
+   let displayedQ = qAndAObject[qDisplayed]
+   questions.innerText = displayedQ.question
+   aOption.innerText = displayedQ.a
+   bOption.innerText = displayedQ.b
+   cOption.innerText = displayedQ.c
+
+// questions.appendChild(qAndAObject.question[0])
+// for(i = 0; i < qAndAObject.question.length; i++) {
+//     console.log(questions)
+}
+
+}
+
+
+);
+
+function uncheckBox() {
+allFormAnswers.forEach(allFormAnswers => allFormAnswers.checked = false)
+}     
+
+submitButton.addEventListener('click', ()=> {
+    let 
+})
