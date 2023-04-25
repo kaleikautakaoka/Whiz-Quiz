58fd4cdbb19f4a583e3b39f71f1efe4327d919e0
kaleikautakaoka <107520051+kaleikautakaoka@users.noreply.github.com>
2023-04-24 22:50:28 -0600

    messages for right and wrong

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ const questionObject = [
       "A progrmming language",
       "An array",
     ],
-    answer: "a",
+    answer: "A",
   },
   {
     qObject: "What is an array?",
@@ -16,7 +16,7 @@ const questionObject = [
       "A progrmming language",
       "A list of stored values and elements",
     ],
-    answer: "c",
+    answer: "C",
   },
   {
     qObject: "What is a class?",
@@ -25,13 +25,13 @@ const questionObject = [
       "A template for creating objects",
       "An array",
     ],
-    answer: "b",
+    answer: "B",
   },
 ];
 
 /*Variables 
 Time and clock variable  index variable to start question loop at first question and answer set*/
-let timeRemaining = questionObject.length * 5;
+let timeRemaining = questionObject.length * 40;
 let index = 0;
 let clockId = 0;
 let allQuestions = questionObject.length;
@@ -67,32 +67,55 @@ question object written to html and for loop to loop through questions
 function createQuizQuestions() {
   $("#questions").html(questionObject[index].qObject);
   $("#listOptionsId").empty();
+  const letterChoice = ["A", "B", "C"]
   for (let i = 0; i < questionObject[index].choices.length; i++) {
     /*What am I doing you ask? I took this li class with the input and label and brought it into the javascript to be revealed once the start button function is clicked*/
     $("#listOptionsId").append(` 
         <li class="list-group-item text-bg-warning">
-        <input class="options" type="checkbox" name="options" value="A" id="a">
-        <label class="form-check-labela" for="a" id="text_Option_a">${questionObject[index].choices[i]}</label>
+        <input class="options" type="checkbox" name="options" value="${letterChoice[i]}" id="${letterChoice[i].toLowerCase()}">
+        <label class="form-check-labela" for="${letterChoice[i].toLowerCase()}" id="text_Option_a">${questionObject[index].choices[i]}</label>
         </li>`);
   }
-  $(".options").on("click", function () {
-    index++;
-    createQuizQuestions();
-  });
   $("#listOptionsId").show();
   $("#hiddenQuestions").show();
+}
+$(document).on("click", "input", function (event) {
+  const userChoice = event.target.value
+
+  console.log(userChoice)
+  console.log(event.target)
+  if (userChoice == questionObject[index].answer) {
+    console.log("correct!");
+    $(".tally").html("Correct!");
+    iterateQuestion()
+  } else {
+    console.log("incorrect");
+    $(".tally").html("Wrong");
+    timeRemaining -= 20
+    iterateQuestion()
+  }
+  
+  
+})
+function iterateQuestion() {
+  index++;
+  if (index < questionObject.length) { 
+    createQuizQuestions();
+  } else {
+    quizComplete();
+  }
 }
 
 
 //function to stop quiz and display new information
-quizComplete();
+// quizComplete();
 function quizComplete() {
-    $("#submitOptionBtn").on("click", function () {
-    clearInterval(clockId);
-    $(".counter").empty();
-    $("#questions").empty();
-    $("#listOptionsId").empty();
-    $("#playerLocal").append(`<div class="clearfix">
+  // $("#submitOptionBtn").on("click", function () {
+  clearInterval(clockId);
+  $(".counter").empty();
+  $("#questions").empty();
+  $("#listOptionsId").empty();
+  $("#playerLocal").append(`<div class="clearfix">
       <div class="card">
       <h2>Your Name</h2>
       <form method="POST">
@@ -106,45 +129,7 @@ function quizComplete() {
       <button id="addBtn" type="button" class="btn btn-warning">Submit Score</button>
       </div>
       </div>`);
-    });
-}
-
-let name = $("#userName").value;
-let score = $("#score").value;
-showUserHistory();
-
-//Local Storage
-$("#playerLocal").on("click", "#addBtn", function (e) {
-  e.preventDefault();
-  console.log("clicked");
-  
-  if (name === "" || score === "") {
-    loggedStorage("error", "Please add your name and score");
-  } else {
-    loggedStorage("success", "Great Work");
-    
-    $("#userName").text = name;
-    $("#score").text = score;
-  }
-  
-  localStorage.setItem("name", name);
-  localStorage.setItem("score", score);
-  showUserHistory();
-  loggedStorage();
-})
-
-function loggedStorage(type, message) {
-  $("displayPastScores").append(name);
-  $("displayPastScores").textContent = message;
-  $("displayPastScores").attr("id", type);
-}
-
-function showUserHistory() {
-  let name = localStorage.getItem("name");
-  let score = localStorage.getItem("score");
+  // });
 }
 
 
-$(document).ready(function () {
-  console.info("ready");
-});
